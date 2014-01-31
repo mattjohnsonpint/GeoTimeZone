58bf4c4fd798146f93ed75d3161a4d32796e63f8
Matt Johnson <mj1856@hotmail.com>
2014-01-30 23:20:39 -0800

    Adding some perf output

--- GeoTimeZone.DataBuilder/Program.cs
+++ GeoTimeZone.DataBuilder/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace GeoTimeZone.DataBuilder
+﻿using System;
+using System.Diagnostics;
+
+namespace GeoTimeZone.DataBuilder
 {
     static class Program
     {
         static void Main()
         {
+            var sw = new Stopwatch();
+            sw.Start();
             TimeZoneDataBuilder.CreateGeohashData(@".\Data\tz_world.shp", @"..\..\..\GeoTimeZone\");
+            sw.Stop();
+            Console.WriteLine("Elapsed Time: {0:g}", sw.Elapsed);
         }
     }
 }
