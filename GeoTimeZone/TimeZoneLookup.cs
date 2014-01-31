8f9b5b2476289e03eb4f8e1c4866729b2b0d0d58
Matt Johnson <mj1856@hotmail.com>
2014-01-30 20:20:18 -0800

    Moving TimeZoneFileReader to it's own file

--- GeoTimeZone/TimeZoneLookup.cs
+++ GeoTimeZone/TimeZoneLookup.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Text;
+﻿using System.IO;
 
 namespace GeoTimeZone
 {
@@ -85,40 +83,6 @@ namespace GeoTimeZone
 
             }
             return 0;
-        }
-    }
-
-    internal class TimezoneFileReader : IDisposable
-    {
-        private const int LineLength = 9;
-        private const int LineEndLength = 1;
-
-        private readonly Stream _stream;
-
-        public TimezoneFileReader()
-        {
-            _stream = typeof (TimezoneFileReader).Assembly.GetManifestResourceStream("GeoTimeZone.TZ.dat");
-            Count = _stream.Length / (LineLength + LineEndLength);
-        }
-
-        public long Count { get; private set; }
-
-        public string GetLine(long line)
-        {
-            var index = ((LineLength + LineEndLength)) * (line - 1);
-
-            _stream.Position = index;
-
-            var buffer = new byte[LineLength];
-
-            _stream.Read(buffer, 0, buffer.Length);
-
-            return Encoding.UTF8.GetString(buffer, 0 , buffer.Length);
-        }
-
-        public void Dispose()
-        {
-            _stream.Dispose();
         }
     }
 }
---
+++ GeoTimeZone/TimezoneFileReader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace GeoTimeZone
+{
+    internal class TimezoneFileReader : IDisposable
+    {
+        private const int LineLength = 9;
+        private const int LineEndLength = 1;
+
+        private readonly Stream _stream;
+
+        public TimezoneFileReader()
+        {
+            _stream = typeof (TimezoneFileReader).Assembly.GetManifestResourceStream("GeoTimeZone.TZ.dat");
+            Count = _stream.Length / (LineLength + LineEndLength);
+        }
+
+        public long Count { get; private set; }
+
+        public string GetLine(long line)
+        {
+            var index = ((LineLength + LineEndLength)) * (line - 1);
+
+            _stream.Position = index;
+
+            var buffer = new byte[LineLength];
+
+            _stream.Read(buffer, 0, buffer.Length);
+
+            return Encoding.UTF8.GetString(buffer, 0 , buffer.Length);
+        }
+
+        public void Dispose()
+        {
+            _stream.Dispose();
+        }
+    }
+}
