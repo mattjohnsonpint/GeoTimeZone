1ee33020b4895a612338f5d5ba516646ceda3159
Simon Bartlett <simon.bartlett@me.com>
2014-02-01 15:54:35 -0500

    More timezone offset tests

--- GeoTimeZone.Tests/TimeZoneLookupTests.cs
+++ GeoTimeZone.Tests/TimeZoneLookupTests.cs
@@ -6,10 +6,33 @@ namespace GeoTimeZone.Tests
     public class TimeZoneLookupTests
     {
         [Test]
-        public void Can_Lookup_Offset_Titantic()
+        public void Can_Lookup_Offset_TitanticWreck()
         {
             var tz = TimeZoneLookup.GetTimeZone(41.7325, -49.9469);
-            Assert.AreEqual("UTC-3", tz);
+            Assert.AreEqual("UTC-03", tz);
+        }
+
+        [Test]
+        public void Can_Lookup_Offset_SsFortLeeWreck()
+        {
+            var tz = TimeZoneLookup.GetTimeZone(-27.583333, 83.183333);
+            Assert.AreEqual("UTC+06", tz);
+        }
+
+        [Test]
+        public void Can_Lookup_Offset_SsLulworthHillWreck()
+        {
+            var tz = TimeZoneLookup.GetTimeZone(-10.166667, 1);
+            Assert.AreEqual("UTC", tz);
+        }
+
+        [Test]
+        public void Can_Lookup_Offset_Extremes()
+        {
+            var tz1 = TimeZoneLookup.GetTimeZone(80, 179);
+            var tz2 = TimeZoneLookup.GetTimeZone(80, -179);
+            Assert.AreEqual("UTC+12", tz1);
+            Assert.AreEqual("UTC-12", tz2);
         }
 
         [Test]
