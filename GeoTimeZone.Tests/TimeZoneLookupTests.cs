8bfa009139065e1cf57b64337907cd59b109c255
Matt Johnson <mj1856@hotmail.com>
2014-02-25 00:44:37 -0800

    Check Windows Time Zone Ids

--- GeoTimeZone.Tests/TimeZoneLookupTests.cs
+++ GeoTimeZone.Tests/TimeZoneLookupTests.cs
@@ -22,6 +22,7 @@ namespace GeoTimeZone.Tests
         {
             var tz = TimeZoneLookup.GetTimeZone(50.4372, -3.5559).Result;
             Assert.AreEqual("Europe/London", tz.IanaTimeZoneId);
+            Assert.AreEqual("GMT Standard Time", tz.WindowsTimeZoneId);
         }
 
         [Test]
@@ -29,6 +30,7 @@ namespace GeoTimeZone.Tests
         {
             var tz = TimeZoneLookup.GetTimeZone(33.45, -112.0667).Result;
             Assert.AreEqual("America/Phoenix", tz.IanaTimeZoneId);
+            Assert.AreEqual("US Mountain Standard Time", tz.WindowsTimeZoneId);
         }
 
         [Test]
@@ -36,6 +38,7 @@ namespace GeoTimeZone.Tests
         {
             var tz = TimeZoneLookup.GetTimeZone(40.67, -73.94).Result;
             Assert.AreEqual("America/New_York", tz.IanaTimeZoneId);
+            Assert.AreEqual("Eastern Standard Time", tz.WindowsTimeZoneId);
         }
 
         [Test]
@@ -43,6 +46,7 @@ namespace GeoTimeZone.Tests
         {
             var tz = TimeZoneLookup.GetTimeZone(34.0500, -118.25).Result;
             Assert.AreEqual("America/Los_Angeles", tz.IanaTimeZoneId);
+            Assert.AreEqual("Pacific Standard Time", tz.WindowsTimeZoneId);
         }
 
         [Test]
@@ -50,6 +54,7 @@ namespace GeoTimeZone.Tests
         {
             var tz = TimeZoneLookup.GetTimeZone(21.3, -157.8167).Result;
             Assert.AreEqual("Pacific/Honolulu", tz.IanaTimeZoneId);
+            Assert.AreEqual("Hawaiian Standard Time", tz.WindowsTimeZoneId);
         }
 
         [Test]
@@ -57,6 +62,7 @@ namespace GeoTimeZone.Tests
         {
             var tz = TimeZoneLookup.GetTimeZone(51.5072, -0.1275).Result;
             Assert.AreEqual("Europe/London", tz.IanaTimeZoneId);
+            Assert.AreEqual("GMT Standard Time", tz.WindowsTimeZoneId);
         }
 
         [Test]
@@ -64,6 +70,7 @@ namespace GeoTimeZone.Tests
         {
             var tz = TimeZoneLookup.GetTimeZone(-23.55, -46.6333).Result;
             Assert.AreEqual("America/Sao_Paulo", tz.IanaTimeZoneId);
+            Assert.AreEqual("E. South America Standard Time", tz.WindowsTimeZoneId);
         }
 
         [Test]
@@ -71,6 +78,7 @@ namespace GeoTimeZone.Tests
         {
             var tz = TimeZoneLookup.GetTimeZone(-33.86, 151.2111).Result;
             Assert.AreEqual("Australia/Sydney", tz.IanaTimeZoneId);
+            Assert.AreEqual("AUS Eastern Standard Time", tz.WindowsTimeZoneId);
         }
 
         [Test]
@@ -78,6 +86,7 @@ namespace GeoTimeZone.Tests
         {
             var tz = TimeZoneLookup.GetTimeZone(-31.55, 159.0833).Result;
             Assert.AreEqual("Australia/Lord_Howe", tz.IanaTimeZoneId);
+            Assert.Null(tz.WindowsTimeZoneId); // No Windows Equivalent
         }
 
         [Test]
@@ -85,6 +94,7 @@ namespace GeoTimeZone.Tests
         {
             var tz = TimeZoneLookup.GetTimeZone(23.22, -106.42).Result;
             Assert.AreEqual("America/Mazatlan", tz.IanaTimeZoneId);
+            Assert.AreEqual("Mountain Standard Time (Mexico)", tz.WindowsTimeZoneId);
         }
     }
 }
