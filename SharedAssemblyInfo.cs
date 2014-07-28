f19f6f1ae826492947a8cac336993a21c574a135
Matt Johnson <mj1856@hotmail.com>
2014-07-27 21:36:22 -0700

    Update version number
    needed to push nuget fix

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -4,5 +4,5 @@
 [assembly: AssemblyProduct("GeoTimeZone")]
 [assembly: AssemblyCopyright("Copyright © 2014, Matt Johnson & Simon Bartlett")]
 
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1.*")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
