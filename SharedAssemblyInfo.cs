02d82df07850e97d848dea8666c70c734d1dc6ec
Matt Johnson <mj1856@hotmail.com>
2014-07-27 21:28:57 -0700

    Update version

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -3,6 +3,6 @@
 [assembly: AssemblyCompany("The GeoTimeZone Authors")]
 [assembly: AssemblyProduct("GeoTimeZone")]
 [assembly: AssemblyCopyright("Copyright © 2014, Matt Johnson & Simon Bartlett")]
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
-[assembly: AssemblyInformationalVersion("0.1.0-beta")]
+
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
