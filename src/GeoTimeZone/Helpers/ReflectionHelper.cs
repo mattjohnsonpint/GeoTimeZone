371f6c765c72d91a73fc9c41eb9b79b8f05a8a83
Marian Platzer <platzer.m@st-sportservice.com>
2016-04-28 13:05:45 +0200

    Upgrade project to dnx451 & dnxcore50

---
+++ src/GeoTimeZone/Helpers/ReflectionHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace GeoTimeZone.Helpers
+{
+    public class ReflectionHelper
+    {
+        public static Assembly GetAssemblyOf(Type type)
+        {
+#if DNX451
+            var assembly = type.Assembly;
+#elif DNXCORE50
+            var assembly = type.GetTypeInfo().Assembly;
+#endif
+            return assembly;
+        }
+    }
+}
--- tests/GeoTimeZone.Tests/NauticalTimeZoneLookupTests.cs
+++ tests/GeoTimeZone.Tests/NauticalTimeZoneLookupTests.cs
@@ -1,37 +1,38 @@
 ﻿using System;
-using NUnit.Framework;
+
+using Xunit;
 
 namespace GeoTimeZone.Tests
 {
-    [TestFixture]
+
     public class NauticalTimeZoneLookupTests
     {
-        [Test]
+        [Fact]
         public void Can_Lookup_Offset_TitanticWreck()
         {
             var tz = TimeZoneLookup.GetTimeZone(41.7325, -49.9469).Result;
-            Assert.AreEqual("Etc/GMT+3", tz);
+            Assert.Equal("Etc/GMT+3", tz);
         }
 
-        [Test]
+        [Fact]
         public void Can_Lookup_Offset_SsFortLeeWreck()
         {
             var tz = TimeZoneLookup.GetTimeZone(-27.583333, 83.183333).Result;
-            Assert.AreEqual("Etc/GMT-6", tz);
+            Assert.Equal("Etc/GMT-6", tz);
         }
 
-        [Test]
+        [Fact]
         public void Can_Lookup_Offset_SsLulworthHillWreck()
         {
             var tz = TimeZoneLookup.GetTimeZone(-10.166667, 1).Result;
-            Assert.AreEqual("UTC", tz);
+            Assert.Equal("UTC", tz);
         }
 
-        [Test]
+        [Fact]
         public void Can_Lookup_Offset_Extreme_East()
         {
             var tz = TimeZoneLookup.GetTimeZone(80, 179).Result;
-            Assert.AreEqual("Etc/GMT-12", tz);
+            Assert.Equal("Etc/GMT-12", tz);
         }
     }
 }
