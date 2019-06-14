550ba5674d0fc85252ff4b948233ef90403f8b5e
Matt Johnson (AZURE) <mj1856@hotmail.com>
2019-06-14 10:27:43 -0700

    Results should be read-only (breaking change).

--- src/GeoTimeZone/TimeZoneResult.cs
+++ src/GeoTimeZone/TimeZoneResult.cs
@@ -1,15 +1,23 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace GeoTimeZone
 {
     public class TimeZoneResult
     {
-        public TimeZoneResult()
+        internal TimeZoneResult(params string[] timeZones)
         {
-            AlternativeResults = new List<string>();
+            if (timeZones.Length == 0)
+            {
+                throw new ArgumentException("There must be at least one value provided.", nameof(timeZones));
+            }
+
+            this.Result = timeZones[0];
+            this.AlternativeResults = new ReadOnlyCollection<string>(timeZones.Skip(1).ToList());
         }
 
-        public string Result { get; set; }
-        public List<string> AlternativeResults { get; set; }
+        public string Result { get; }
+        public ReadOnlyCollection<string> AlternativeResults { get; }
     }
 }
