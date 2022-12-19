7347df451c2e64fad099b3e29787c741020df277
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-12-19 15:07:59 -0800

    Update data to 2022g

--- src/GeoTimeZone.DataBuilder/Program.cs
+++ src/GeoTimeZone.DataBuilder/Program.cs
@@ -12,7 +12,7 @@ internal static class Program
 
         ConsoleOutput.WriteMessage("Downloading Time Zone Boundaries Shapefile");
         var tempDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName()[..8]);
-        var filePath = await DownloadDataAsync("2021c", tempDir);
+        var filePath = await DownloadDataAsync("2022g", tempDir);
 
         ConsoleOutput.WriteMessage("Extracting contents...");
         ZipFile.ExtractToDirectory(filePath, tempDir);
